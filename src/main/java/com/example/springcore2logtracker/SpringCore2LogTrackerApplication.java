d2eee2e6e3233bf262c83acf094cdb01edb90395
eunjune <iws1605@naver.com>
2022-11-05 22:47:05 +0900

    Initial commit

---
+++ src/main/java/com/example/springcore2logtracker/SpringCore2LogTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcore2logtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCore2LogTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCore2LogTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcore2logtracker/SpringCore2LogTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcore2logtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCore2LogTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
