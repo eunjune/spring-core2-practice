75b38ceabebc8487a161b5cd01e7eee38d6725f0
eunjun <eun-jun.jeong@worksmobile.com>
2022-11-26 00:07:38 +0900

    쓰레드 로컬 적용

--- src/main/java/com/example/springcore2logtracker/LogTraceConfig.java
+++ src/main/java/com/example/springcore2logtracker/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.example.springcore2logtracker;
 
-import com.example.springcore2logtracker.trace.logtrace.FieldLogTrace;
 import com.example.springcore2logtracker.trace.logtrace.LogTrace;
+import com.example.springcore2logtracker.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +10,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
