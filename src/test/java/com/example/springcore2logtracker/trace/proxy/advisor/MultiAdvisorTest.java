cecfaa637a8495a57b588e7df79442d82610b595
eunjun <eun-jun.jeong@worksmobile.com>
2022-12-30 19:27:23 +0900

    여러 어드바이저 적용하기

---
+++ src/test/java/com/example/springcore2logtracker/trace/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,67 @@
+package com.example.springcore2logtracker.trace.proxy.advisor;
+
+import com.example.springcore2logtracker.trace.proxy.common.service.ServiceImpl;
+import com.example.springcore2logtracker.trace.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+public class MultiAdvisorTest {
+
+    @Test
+    @DisplayName("여러 프록시")
+    public void multiAdvisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory.getProxy();
+
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        proxy2.save();
+    }
+
+    // client -> proxy -> advisor2 -> advisor1 -> target
+    @Test
+    @DisplayName("하나의 프록시에 여러 어드바이저")
+    public void multiAdvisorTest2() {
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        proxyFactory.addAdvisor(advisor2);
+        proxyFactory.addAdvisor(advisor1);
+
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy1.save();
+    }
+
+    static class Advice1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    static class Advice2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}
