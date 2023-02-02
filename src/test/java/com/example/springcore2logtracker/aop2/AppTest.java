8ad6a2a31171d01a7c4ee9d8d0a33ff96fad4fb9
eunjun <eun-jun.jeong@worksmobile.com>
2023-02-02 22:44:05 +0900

    예제 프로젝트 세팅

---
+++ src/test/java/com/example/springcore2logtracker/aop2/AppTest.java
@@ -0,0 +1,46 @@
+package com.example.springcore2logtracker.aop2;
+
+import com.example.aop2.order.OrderRepository;
+import com.example.aop2.order.OrderService;
+import com.example.aop2.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+public class AppTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    public void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    public void exception() {
+        orderService.orderItem("ex");
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+
+
+    }
+
+
+
+}
