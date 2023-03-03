baac19248cb89642f86a818877aea47aefcf856a
eun-jun <eun-jun.jeong@worksmobile.com>
2023-03-03 18:03:40 +0900

    프록시와 내부 호출 - 대안1 자기 자신 주입

---
+++ src/main/java/com/example/aop2/internalcall/CallServiceV1.java
@@ -0,0 +1,27 @@
+package com.example.aop2.internalcall;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+	private CallServiceV1 callServiceV1;
+
+	@Autowired
+	public void setCallServiceV1(CallServiceV1 callServiceV1) {
+		this.callServiceV1 = callServiceV1;
+	}
+
+	public void external() {
+		log.info("call external");
+		callServiceV1.internal();
+	}
+
+	public void internal() {
+		log.info("call internal");
+	}
+}
